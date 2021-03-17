93e02e47e35af896f5f87ba605f3065f7ccf1308
rem42 <me@remy.ovh>
2021-03-17 18:59:26 +0100

    fix(scraper): add product bundle getter/setter

--- src/Entity/PrestashopProductBundle.php
+++ src/Entity/PrestashopProductBundle.php
@@ -9,4 +9,40 @@ final class PrestashopProductBundle
     private ?int $idProductAttribute = null;
 
     private ?int $quantity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getIdProductAttribute(): ?int
+    {
+        return $this->idProductAttribute;
+    }
+
+    public function setIdProductAttribute(?int $idProductAttribute): self
+    {
+        $this->idProductAttribute = $idProductAttribute;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
 }
