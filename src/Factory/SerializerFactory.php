71d8fa47daaaf674f811a867a800954c8be50d3f
rem42 <me@remy.ovh>
2024-02-20 22:27:34 +0100

    feat: upgrade serializer

--- src/Factory/SerializerFactory.php
+++ src/Factory/SerializerFactory.php
@@ -2,14 +2,13 @@
 
 namespace Scraper\ScraperPrestashop\Factory;
 
-use Doctrine\Common\Annotations\AnnotationReader;
 use Scraper\ScraperPrestashop\Normalizer\PrestashopDeliveryPointNormalizer;
 use Scraper\ScraperPrestashop\Normalizer\PrestashopItemNormalizer;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
@@ -22,7 +21,7 @@ class SerializerFactory
 {
     public static function create(): Serializer
     {
-        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
 
         $metadataAwareNameConverter = new MetadataAwareNameConverter($classMetadataFactory, new CamelCaseToSnakeCaseNameConverter());
 
